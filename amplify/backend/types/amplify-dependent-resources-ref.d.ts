10d51453f47bc86e20e3521096999f492b2d73d9
Valentin <wdcvalentin@yahoo.fr>
2023-03-14 10:50:06 +0100

    feat: getUser lambda

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,5 +1,12 @@
 export type AmplifyDependentResourcesAttributes = {
   "function": {
+    "amplifyGetUser": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "amplifyadduser": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
