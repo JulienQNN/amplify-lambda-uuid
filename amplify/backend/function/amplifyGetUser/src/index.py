92252a0c0b844b9bb41c023bab2bba69587889c1
Julien <julien.quennehen@edu.devinci.fr>
2023-03-14 19:26:54 +0100

    feat: 🎸 put and retrieve tokens from AWS Secrets Manager
    API Gateway + Lambda + DynamoDB + Secrets Manager

--- amplify/backend/function/amplifyGetUser/src/index.py
+++ amplify/backend/function/amplifyGetUser/src/index.py
@@ -6,16 +6,17 @@ import uuid
 from boto3.dynamodb.conditions import Attr
 
 dynamodb = boto3.resource('dynamodb')
-
+secretmanager = boto3.client('secretsmanager')
 
 def handler(event, context):
     table = dynamodb.Table('user-dev')
-    #body = json.loads(event['body'])
-    body = event['body']
-    print(body)
+    body = json.loads(event['body'])
+    #body = event['body']
     response = get_user(table, body)
-    
-    return {
+       
+    if (response):
+       print("la reponse" + response)
+       return {
         'statusCode': 200,
         'headers': {
             'Access-Control-Allow-Headers': '*',
@@ -24,26 +25,82 @@ def handler(event, context):
         },
         'body': response
     }
-
+        
+        
 
 def get_user(table, body):
-    print("==== get user ====")
     try:
         data = scan(table, filter_expression=Attr(
-            'lastname').eq(body['lastname']), projection='firstname, lastname, age')
-
+            'lastname').eq(body['lastname']), projection='firstname, lastname, age, id')
+       
         if (data):
-            return 'Oui il existre'
-
-        return 'Non il existe pas'
+            data = data[0]
+            secret = get_secret("userSecret", data['id'])
+            
+            return secret
+        else:
+            id = insert_user(table, body)
+            secret = create_secret("userSecret", id)
+            
+            return secret
+            
 
     except KeyError:
         data = 'User not found'
+    
+    
 
-    return data
 
+def insert_user(table, body):
+ 
+    id = str(uuid.uuid4())
+    table.put_item(Item={
+      'id': id,
+      'age': str(body['age']),
+      'firstname': str(body['firstname']),
+      'lastname': str(body['lastname'])
+      
+    })
+    
+    return id
 
+def create_secret(secret_name, id):
+    secret_object = secretmanager.get_secret_value(SecretId=secret_name)
+   
+    secretuuid4 = str(uuid.uuid4())
+    if secret_object:
 
+     secret_global_object = json.loads(secret_object["SecretString"])
+     secret_global_object[secretuuid4] = id
+     secret = json.dumps(secret_global_object)
+     
+     secretmanager.put_secret_value(
+         SecretId= "userSecret",
+         SecretString=secret,
+     )
+     
+
+    else:
+        secretmanager.create_secret(
+            Name= "userSecret",
+            SecretString=json.dumps({
+                secret : id
+            })
+    )
+    return secretuuid4
+    
+    
+    
+def get_secret (secret_name, secret_key):
+
+  secret_object = secretmanager.get_secret_value(SecretId=secret_name)
+
+  secret_values = json.loads(secret_object["SecretString"])
+  
+  for key in secret_values:
+      if secret_values[key] == secret_key:
+          return key
+     
 
 def scan(table, filter_expression, projection):
     data = []
